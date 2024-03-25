9f63ad1ab779689ed7a3204a682be3a551f9ca91
kownhee <qkrrnjsgml7@naver.com>
2024-03-26 00:41:51 +0900

    240326 TIL
    - 코딩테스트 연습

---
+++ programmers/240326_3.js
@@ -0,0 +1,7 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120912
+// 코딩테스트 연습 > 코딩테스트 입문 > 7의 개수
+
+function solution(array) {
+    return array.join('').split('7').length-1;
+}
+
